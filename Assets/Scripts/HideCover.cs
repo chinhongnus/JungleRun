71f21b584aa3fbf898ac7d40d84a0f514ebc927d
Ben <idmlch@nus.edu.sg>
2018-09-21 10:21:08 +0800

    One more polish :p

---
+++ Assets/Scripts/HideCover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCover : MonoBehaviour {
+
+	public GameObject coverPanel;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if (coverPanel != null)
+		{
+			coverPanel.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
